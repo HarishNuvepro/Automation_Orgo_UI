e35c5ca6d908b6dae5d3a9efd7351d7f0cf10c6f
Harish Kumar <harish@nuvepro.com>
2026-04-14 10:48:56 +0530

    modified ConstantFilePath.java

--- src/main/java/Generic_Utility/ConstantFilePath.java
+++ src/main/java/Generic_Utility/ConstantFilePath.java
@@ -4,8 +4,10 @@ public interface ConstantFilePath {
 
 	String excelfilepath = ".\\src\\main\\resources\\TestData_Trail.xlsx";
 
+	String geminiApiKeyBase64 = "QUl6YXNTeURJMnpTN2Q3NTdHUH45U01aQ1pQaXAwVUE3ZXduRTk4";
+	
 	String geminiApiKey = System.getenv("GEMINI_API_KEY") != null
 			? System.getenv("GEMINI_API_KEY")
-			: "AIzaSyDI6zS2dL757GPn1SMzCZPip0VA7ewnE98";
+			: new String(java.util.Base64.getDecoder().decode(geminiApiKeyBase64));
 
 }
